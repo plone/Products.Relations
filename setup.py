14159dbc10bc5ec29ce86e005c9f9f24ca6f66a8
Alex Clark <aclark@aclark.net>
2008-05-21 00:10:20 +0000

    Prepare to move Relations trunk to egg.
    svn path=/Products.Relations/trunk/; revision=9742

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.8 (svn/trunk)'
+
+setup(name='Products.Relations',
+      version=version,
+      description="Relations allows for the definition of sets of rules for validation, creation and lifetime of Archetypes references.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Daniel Nouri',
+      author_email='daniel (dot) nouri at gmail (dot) com',
+      url='http://svn.plone.org/svn/archetypes/Products.Relations',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
